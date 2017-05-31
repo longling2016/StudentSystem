205b8e0b9f350c92dc1ad13f2200ebe0dd2e5d9f
longling2016 <longling.wang2016@gmail.com>
2017-05-30 18:11:21 -0700

    debug

--- students.rb
+++ students.rb
@@ -76,16 +76,22 @@ post '/students' do
 end
 
 put '/students/:studentID' do
+  @cur_student = params[:studentID]
   if Student.get(params[:student][:studentID]).nil? || params[:student][:studentID].eql?(params[:studentID])
-    if params[:student][:firstname].gsub(/\W+/, '').eql?('') || params[:student][:lastname].gsub(/\W+/, '').eql?('')
-      @cur_student = params[:studentID]
+    firstname = params[:student][:firstname]
+    lastname = params[:student][:lastname]
+    if firstname.eql?('') || lastname.eql?('') || firstname.gsub(/\W+/, '').eql?('') || lastname.gsub(/\W+/, '').eql?('')
       erb :wrong_date
     else
-      student = Student.get(params[:studentID])
-      student.update(params[:student])
-      puts 'db_log: hitting put method'
-      puts student.studentID
-      redirect to("/students/#{student.studentID}")
+      begin
+        Date.strptime(params[:student][:birthday], "%m/%d/%Y")
+        student = Student.get(params[:studentID])
+        student.update(params[:student])
+        redirect to("/students/#{student.studentID}")
+      rescue ArgumentError
+        erb :wrong_date
+      end
+
     end
   else
     erb :wrong_date
